3bbc9ef84e060990ff803d20dfc4bbd61de4c448
Fernando Salas <luisfernandosalasg@gmail.com>
2023-06-19 06:40:54 -0500

    Add Basic config

---
+++ src/main/java/net/fernandosalas/springboot/SpringbootSearchRestApiApplication.java
@@ -0,0 +1,13 @@
+package net.fernandosalas.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSearchRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSearchRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/fernandosalas/springboot/SpringbootSearchRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fernandosalas.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSearchRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
