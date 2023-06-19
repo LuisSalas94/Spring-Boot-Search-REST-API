54fe3254b3f6a67a535bb3d9dbcbe8c168eeb0e7
Fernando Salas <luisfernandosalasg@gmail.com>
2023-06-19 07:11:33 -0500

    Create JPQL and SQL Queries

--- src/main/java/net/fernandosalas/springboot/repository/ProductRepository.java
+++ src/main/java/net/fernandosalas/springboot/repository/ProductRepository.java
@@ -1,7 +1,19 @@
 package net.fernandosalas.springboot.repository;
-
 import net.fernandosalas.springboot.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Using JPQL (Java Persistence Query Language)
+    @Query("SELECT p FROM Product p WHERE " +
+            "p.name LIKE CONCAT('%',:query, '%')" +
+            "OR p.description LIKE CONCAT('%',:query, '%')")
+    List<Product> searchProducts(String query);
+
+    // Native SQL
+    @Query(value = "SELECT * FROM Product p WHERE " +
+            "p.name LIKE CONCAT('%',:query, '%')" +
+            "OR p.description LIKE CONCAT('%',:query, '%')", nativeQuery = true)
+    List<Product> searchProductsSQL(String query);
 }
